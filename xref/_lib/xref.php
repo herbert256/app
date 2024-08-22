89d32daed024ec264003b47a751f751ff6bbfc68
herbert <herbert@groot.jebbink.nl>
2024-08-22 07:11:54 +0200

    Automatic update

--- xref/_lib/xref.php
+++ xref/_lib/xref.php
@@ -14,6 +14,9 @@
       return $items;
     }
 
+    if ( $xref == '*') 
+      return getXrefDir ( $type, $dir );
+    
     $directory = new DirectoryIterator ( "/pad/$dir" );
     $iterator  = new IteratorIterator  ( $directory );
 
@@ -25,14 +28,10 @@
       $ext  = $one->getExtension();
       $file = $one->getFilename();
 
-      if ( $type <> 'Sequences' )
-        if ( $ext <> 'pad' and $ext <> 'php' ) 
-          continue;
+      if ( $ext <> 'pad' and $ext <> 'php' ) 
+        continue;
 
-      if ( $type == 'Sequences' )
-        $item = $one->getBasename();
-      else
-        $item = substr($file, 0, strrpos($file, '.')   );
+      $item = substr($file, 0, strrpos($file, '.')   );
 
       if ( ! $item       ) continue;
       if ( $file == '.'  ) continue;
@@ -46,6 +45,38 @@
         $items [$item] ['dir'] = "$xref/$item";
       elseif ( file_exists ( "/app/_xref/$xref/$item.txt" ) )
         $items [$item] ['pages'] = "$xref/$item.txt";
+
+    }
+   
+    ksort ( $items );
+    return $items;
+
+  }
+
+  function getXrefDir ( $type, $dir ) {
+    
+    $base = "/app/_xref/";
+
+    $directory = new DirectoryIterator ( "$base$dir" );
+    $iterator  = new IteratorIterator  ( $directory );
+
+    $items = [];
+    
+    foreach ($iterator as $one) {
+
+      $path = padCorrectPath ( $one->getPathname() );
+      $ext  = $one->getExtension();
+      $file = $one->getFilename();
+
+      $item = substr($file, 0, strrpos($file, '.')   );
+
+      if ( ! $item       ) continue;
+      if ( $file == '.'  ) continue;
+      if ( $file == '..' ) continue;
+
+      $items [$item] ['item']  = $item;
+      $items [$item] ['dir']   = '';
+      $items [$item] ['pages'] = "$dir/$file";
 
     }
    
--- xref/pages.php
+++ xref/pages.php
@@ -6,12 +6,6 @@
 
   $go = file ( "/app/_xref/$pages", FILE_IGNORE_NEW_LINES );
 
-  if ( count ($go) == 1 )
-    padRedirect ( 'xref/go',
-                  [ 'go'   => $go [0],
-                    'type' => $type ?? '',
-                    'item' => $item ?? ''] );
-
   $title .= " - $type - $item";
 
 ?>
