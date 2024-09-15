ed4c3823a82e60bfcaf0b4f60b0c6ffd5adab5ff
herbert <herbert@groot.jebbink.nl>
2024-09-15 13:39:42 +0200

    Automatic update

--- manual/sequence/examples.php
+++ manual/sequence/examples.php
@@ -1,7 +1,7 @@
 <?php
 
   if ( ! isset ( $example ) or ! $example )
-    $example = 'sequences';
+    $example = 'specials';
 
   if ( $example == 'sequences')
     $examples = seqDir ( "/app/sequence/basic" ) ;
