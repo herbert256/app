c2e26e3067e600ad1bc21ce7230d6085c2cd60a4
herbert <herbert@groot.jebbink.nl>
2024-10-26 08:20:51 +0200

    Automatic update

--- test.php
+++ test.php
@@ -1,15 +1,15 @@
 <?php
  
-  foreach ( glob ( "/pad/functions/*.php") as $file)  
+  foreach ( glob ( "/xxxxxpad/functions/*.php") as $file)  
 
   	if ( str_contains ( file_get_contents ( $file), '$parm' ) ) {
-      $new = str_replace ( 'functions/', 'functions/parms', $file);
+      $new = str_replace ( 'functions/', 'functions/parms/', $file);
       copy ( $file, $new );
-	  file_put_contents ( $filename, "<?php\n\n  return include '$new';\n\n?>")
+	  file_put_contents ( $file, "<?php\n\n  return include '$new';\n\n?>");
   	} else {
-      $new = str_replace ( 'functions/', 'functions/single', $file);
+      $new = str_replace ( 'functions/', 'functions/single/', $file);
       copy ( $file, $new );
-	  file_put_contents ( $filename, "<?php\n\n  return include '$new';\n\n?>")
+	  file_put_contents ( $file, "<?php\n\n  return include '$new';\n\n?>");
   	}
 
 ?>
